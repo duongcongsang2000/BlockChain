17f10ffa2c69c9675c121b8186beb60a94363153
Sang <duongcongsang2000@gmail.com>
2023-06-30 14:39:42 +0700

    update

--- chain-util.js
+++ chain-util.js
@@ -2,7 +2,8 @@ const EC = require('elliptic').ec;
 const SHA256 = require('crypto-js/sha256');
 const uuidV1 = require('uuid/v1');
 const ec = new EC('secp256k1');
-
+/* a 
+*/
 class ChainUtil {
   static genKeyPair() {
     return ec.genKeyPair();
